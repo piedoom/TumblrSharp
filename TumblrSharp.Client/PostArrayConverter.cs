6d7e252c711f124d8cadd93de30192243239b715
Alexander Lozada <alexanderpaullozada@gmail.com>
2016-08-19 13:28:21 -0400

    re-added submission fix

--- TumblrSharp.Client/PostArrayConverter.cs
+++ TumblrSharp.Client/PostArrayConverter.cs
@@ -60,6 +60,10 @@ namespace DontPanic.TumblrSharp.Client
                     case "video":
 						list.Add(jo.ToObject<VideoPost>());
                         break;
+
+                    case "submission":
+                        list.Add(jo.ToObject<AnswerPost>());
+                        break;
 						
 					default:
 						list.Add(jo.ToObject<AnswerPost>());

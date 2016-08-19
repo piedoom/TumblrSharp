324d1b5f85fccf361a03ef806dcfc2148898a992
Alexander Lozada <alexanderpaullozada@gmail.com>
2016-08-19 13:05:50 -0400

    Added nuget upload

--- TumblrSharp/Properties/AssemblyInfo.cs
+++ TumblrSharp/Properties/AssemblyInfo.cs
@@ -2,17 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tumblr#")]
+[assembly: AssemblyTitle("New TumblrSharp")]
 [assembly: AssemblyDescription("A Tumblr API v2 client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Don't Panic Software")]
+[assembly: AssemblyCompany("Don't Panic Software, Vaporsoft")]
 [assembly: AssemblyProduct("TumblrSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2016 Don't Panic Software, Doomy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 [assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("Newtonsoft.Json")]

fc9db823cbdd960f572fa1f372d7dfed9bd7bc9a
Cataurus <cataurusfynn@yahoo.de>
2018-05-17 01:53:46 +0200

    1.1.39
    add trial support
    add exif support for photopost

---
+++ TumblrSharp.Client/Trial.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace DontPanic.TumblrSharp.Client
+{
+    public class Trial
+    {
+        [JsonProperty(PropertyName = "blog")]
+        public TrialBlog Blog { get; set; }
+
+        [JsonProperty(PropertyName ="post")]
+        public TrialPost Post { get; set; }
+
+        [JsonProperty(PropertyName = "content_raw")]
+        public string ContentRaw { get; set; }
+
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+    }
+}

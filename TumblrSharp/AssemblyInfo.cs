82ac20be62e94b28c0744f674ee415f8135cb20a
Cataurus <cataurusfynn@yahoo.de>
2018-09-19 16:07:15 +0200

    fix package for .Netstandard 1.1 and appveyor

---
+++ TumblrSharp/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Alexander Lozada; Ulf (Cataurus) Prill; et al.")]
+[assembly: AssemblyCopyright("Copyright 2017-2018")]
+[assembly: AssemblyDescription("Tumblr# extension for .NETStandard 1.1, 1.2, 1.3 and 2.0")]
+[assembly: AssemblyFileVersion("1.1.115.0")]
+[assembly: AssemblyInformationalVersion("1.1.115.0")]
+[assembly: AssemblyProduct("DontPanic.TumblrSharp")]
+[assembly: AssemblyTitle("DontPanic.TumblrSharp")]
+[assembly: AssemblyVersion("1.1.115.0")]

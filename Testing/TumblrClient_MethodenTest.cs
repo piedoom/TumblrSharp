12a91991a628068fd0e2f021d589e472547d73b4
Ulf Prill <cataurusfynn@yahoo.de>
2018-12-12 10:08:42 +0100

    added new tests

--- Testing/TumblrClient_MethodenTest.cs
+++ Testing/TumblrClient_MethodenTest.cs
@@ -13,7 +13,7 @@ namespace Testing
     [TestClass]
     public class TumblrClient_MethodenTest
     {
-        // This consumer-token and AccesToken is only for testing!
+        // This ConsumerToken and AccessToken is only for testing!
 
         private readonly string _consumerKey = "hGSKqgb24RJBnWkodL5GTFIeadgyOnWl0qsXi7APRC76HELnrE";
         private readonly string _consumerSecret = "jdNWSSbG7bZ8tYJcYzmyfH33o5cq7ihmJeWMVntB3pUHNptqn3";
@@ -41,6 +41,84 @@ namespace Testing
             tc.Dispose();
 
             var userInfo = await tc.GetUserInfoAsync();    
+        }
+
+        [TestMethod]
+        public async Task UserInfo_1()
+        {
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+
+            var userInfo = await tc.GetUserInfoAsync();
+
+            Assert.AreEqual(userInfo.Blogs[1].Name, "newtsharp");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task CreatePost_Required_1()
+        {
+            string version = typeof(TumblrClient).Assembly.GetName().Version.ToString();
+
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+
+            PostData postData = PostData.CreateText("this is a textpost test. NewTumblrSharp " + version, "Testpost", new List<string> { "NewTumblrSharp", "Test" });
+
+            PostCreationInfo postCreationInfo = await tc.CreatePostAsync("", postData);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public async Task CreatePost_Required_2()
+        {
+            string version = typeof(TumblrClient).Assembly.GetName().Version.ToString();
+
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+
+            PostData postData = PostData.CreateText("this is a textpost test. NewTumblrSharp " + version, "Testpost", new List<string> { "NewTumblrSharp", "Test" });
+
+            PostCreationInfo postCreationInfo = await tc.CreatePostAsync("test", postData);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task CreatePost_Required_3()
+        {
+            string version = typeof(TumblrClient).Assembly.GetName().Version.ToString();
+
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+                        
+            UserInfo userInfo = await tc.GetUserInfoAsync();
+
+            PostCreationInfo postCreationInfo = await tc.CreatePostAsync(userInfo.Blogs[0].Name, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task CreatePost_Required_4()
+        {
+            string version = typeof(TumblrClient).Assembly.GetName().Version.ToString();
+
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+
+            UserInfo userInfo = await tc.GetUserInfoAsync();
+
+            PostCreationInfo postCreationInfo = await tc.CreatePostAsync("newtsharp.tumblr.com", PostData.CreateText());
+        }
+
+        [TestMethod]
+        public async Task CreatePost_Text()
+        {
+            string version = typeof(TumblrClient).Assembly.GetName().Version.ToString();
+
+            tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
+
+            PostData postData = PostData.CreateText("This is a textpost test.\n\n Package NewTumblrSharp " + version, "Testpost", new List<string> {"NewTumblrSharp", version});
+                        
+            PostCreationInfo postCreationInfo = await tc.CreatePostAsync("newtsharp.tumblr.com", postData);
+
+            Assert.IsNotNull(postCreationInfo);
+
+            Assert.AreNotEqual(postCreationInfo.PostId, 0);                        
         }
     }
 }
